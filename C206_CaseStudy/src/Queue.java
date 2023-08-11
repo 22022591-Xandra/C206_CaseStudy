6aae8e0d5496b2c3072e3ebd531cad3cf99d13f7
22013254-Fikriyah <22013254@myrp.edu.sg>
2023-08-11 10:04:03 +0800

    fikriyah

---
+++ C206_CaseStudy/src/Queue.java
@@ -0,0 +1,39 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Fiqriyah, 11 Aug 2023 10:03:19 am
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Fiqriyah, 11 Aug 2023 10:03:19 am
+ */
+
+public class Queue {
+  private String userName;
+  private String itemname;
+  private int qNumber;
+  
+  
+  public Queue(String userName, int qNumber) {
+    this.userName = userName;
+    this.qNumber = qNumber;
+  }
+  public String getUserName() {
+    return userName;
+  }
+  public String getItemname() {
+    return itemname;
+  }
+  public int getQNumber() {
+    return qNumber;
+  }
+  public void setQNumber(int newQNumber) {
+    qNumber = newQNumber;
+  }
+}
